4ffd17c50dcb64efb8acbd0f86ce195dac2edaa6
Mohammad <mohammadhammad50@gmail.com>
2022-08-12 22:14:28 -0600

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...itemsToBeRemoved) {
+    return array.filter(
+        (item) => {
+            return !itemsToBeRemoved.includes(item);
+        }
+    );
 };
 
 // Do not edit below this line
