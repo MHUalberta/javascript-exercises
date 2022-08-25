2cc0dd62665546d6b8080dcd12c6239b47ce9bef
Mohammad <mohammadhammad50@gmail.com>
2022-08-25 16:53:53 -0600

    Solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, curr) => sum+curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((mult, curr) => mult*curr);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let factorial = 1;
+  for (let i = 0; i < a; i++)
+    factorial *= a-i;
+  return factorial;
 };
 
 // Do not edit below this line
