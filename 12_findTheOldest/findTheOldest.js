d1a8795c5e504ee0f4ef1a5eb2bb28c51c5a373b
Mohammad <mohammadhammad50@gmail.com>
2022-08-26 08:37:09 -0600

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+        return array.reduce((oldest, current) => {
+            let oldestAge;
+            let currentAge;
+            if (!oldest.yearOfDeath) {
+                const currentYear = new Date().getFullYear();
+                oldestAge = currentYear - oldest.yearOfBirth;
+            }
+            else {
+                oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+            }
 
-};
+            if (!current.yearOfDeath) {
+                const currentYear = new Date().getFullYear();
+                currentAge = currentYear - current.yearOfBirth;
+            }
+            else {
+                currentAge = current.yearOfDeath - current.yearOfBirth;
+            }
+
+            return oldestAge > currentAge ? oldest: current;
+        });
+    };
 
 // Do not edit below this line
 module.exports = findTheOldest;
