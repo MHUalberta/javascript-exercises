4b215a87e1bfad744942877ecbe84cf6e60a7d21
Mohammad <mohammadhammad50@gmail.com>
2022-08-25 22:14:21 -0600

    Solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[^a-z]/g, "");
+    let reversedString = string.split("").reverse().join("");
+    return string === reversedString;
 };
 
 // Do not edit below this line
