8dc74cb090fb194386c2b47eeef0a1630fb25a3c
Mohammad <mohammadhammad50@gmail.com>
2022-08-25 22:29:23 -0600

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(fib) {
+    fib = + fib;
+    if (fib < 0) return "OOPS";
+    else if (fib <= 1) return fib;
+    else return fibonacci(fib-1) + fibonacci(fib-2);
 };
 
 // Do not edit below this line
